5f9ae1e55821bc9021f9addd8fcfe7673c9518b8
dodt99 <tiendo220199@gmail.com>
2026-04-01 15:52:22 +0700

    fix: use explicit wrapper function instead of re-export for modal page

--- app/exercises/intercept/@modal/(.)photo/[id]/page.tsx
+++ app/exercises/intercept/@modal/(.)photo/[id]/page.tsx
@@ -4,4 +4,10 @@ export function generateStaticParams() {
   return ["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((id) => ({ id }));
 }
 
-export default PhotoModalClient;
+export default function PhotoModalPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  return <PhotoModalClient params={params} />;
+}
